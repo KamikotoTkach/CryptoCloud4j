8c7a3ec8992409f03707fc10d776fe2faeef7cf9
DESKTOP\PC <tkach@cwcode.ru>
2024-08-22 15:34:01 +0300

    add javadocs to CreateBillRequest, BillCreated

--- src/main/java/ru/cwcode/tkach/cryptocloud4j/model/request/CreateBillRequest.java
+++ src/main/java/ru/cwcode/tkach/cryptocloud4j/model/request/CreateBillRequest.java
@@ -15,47 +15,83 @@ public class CreateBillRequest extends CryptoCloudRequest<CreateBillRequest> {
     addResponseModel(200, BillCreated.class);
   }
   
+  /**
+   * @param locale currently `en` or `ru`
+   */
   public CreateBillRequest locale(String locale) {
     addParameter("locale", locale);
     return this;
   }
   
+  /**
+   * @param shopId Unique identifier of the store from personal account. Required.
+   */
   public CreateBillRequest shopId(String shopId) {
     addBody("shop_id", shopId);
     return this;
   }
   
+  /**
+   * @param amount Payment amount in USD. Required.
+   */
   public CreateBillRequest amount(Double amount) {
     addBody("amount", amount);
     return this;
   }
   
+  /**
+   * @param currency Available currencies for conversion: USD, UZS, KGS, KZT, AMD, AZN, BYN, AUD, 'TRY', AED, CAD, CNY, HKD, IDR, INR, INR, JPY, PHP, SGD, THB, VND, MYR, RUB, UAH, EUR, GBP. The default currency is USD
+   */
   public CreateBillRequest currency(String currency) {
     addBody("currency", currency);
     return this;
   }
   
+  /**
+   * @param orderId Arbitrary invoice number in the external system.
+   */
   public CreateBillRequest orderId(String orderId) {
     addBody("order_id", orderId);
     return this;
   }
   
+  /**
+   * @param email Email address of the customer
+   */
   public CreateBillRequest email(String email) {
     addBody("email", email);
     return this;
   }
   
-  public CreateBillRequest email(Fields fields) {
+  /**
+   * @param fields Additional parameters
+   */
+  public CreateBillRequest addFields(Fields fields) {
     addBody("add_fields", fields);
     return this;
   }
   
   @Getter
   public static class Fields extends ModelObject {
+    /**
+     * Invoice due time
+     */
     ModelField<Time> timeToPay = new ModelField<>("time_to_pay", Time.class, this);
+    /**
+     * If you specify an email in the parameter, the invoice will be automatically sent to this email.
+     */
     ModelField<String> emailToSend = new ModelField<>("email_to_send", String.class, this);
+    /**
+     * Payment currencies
+     */
     ModelField<String[]> availableCurrencies = new ModelField<>("available_currencies", String[].class, this);
+    /**
+     * Select the payment currency for the user. The details will be displayed on the payment page when specifying the currency. The address will be given in response to the request.
+     */
     ModelField<String> cryptocurrency = new ModelField<>("cryptocurrency", String.class, this);
+    /**
+     * Period for invoice resubmission (only if email for automatic invoice sending is filled in).
+     */
     ModelField<String> period = new ModelField<>("period", String.class, this);
     
     public Fields() {
--- src/main/java/ru/cwcode/tkach/cryptocloud4j/model/response/BillCreated.java
+++ src/main/java/ru/cwcode/tkach/cryptocloud4j/model/response/BillCreated.java
@@ -10,27 +10,92 @@ import java.util.Map;
 
 @Getter
 public class BillCreated extends ModelObject {
+  /**
+   * unique identifier of the invoice with the INV prefix.
+   */
   ModelField<String> uuid = new ModelField<>("uuid", String.class, this);
+  /**
+   * time of invoice creation in UTC +0 in the format YYYYY-MM-DD HH:MI:SS.FFFFFF.
+   */
   ModelField<String> created = new ModelField<>("created", String.class, this);
+  /**
+   * payment address. It will be pre-filled only in case of selecting cryptocurrency for payment.
+   * To select the currency for the client, it is necessary to specify the currency
+   * available in the project in add_fields.cryptocurrency.
+   */
   ModelField<String> address = new ModelField<>("address", String.class, this);
+  /**
+   * expiration date of the invoice in UTC +0 in the format YYYY-MM-DD HH:MI:SS.FFFFFFFF.
+   * After the expiry date invoice will be transferred to the status canceled, if no payment has been received.
+   */
   ModelField<String> expiryDate = new ModelField<>("expiry_date", String.class, this);
+  /**
+   * transfer fee payment side
+   */
   ModelField<String> sideCommission = new ModelField<>("side_commission", String.class, this);
+  /**
+   * service fee payment side.
+   */
   ModelField<String> sideCommission_cc = new ModelField<>("side_commission_cc", String.class, this);
+  /**
+   * invoice amount.
+   */
   ModelField<Double> amount = new ModelField<>("amount", Double.class, this);
+  /**
+   *  invoice amount in USD.
+   */
   ModelField<Double> amountUsd = new ModelField<>("amount_usd", Double.class, this);
+  /**
+   * invoice amount in creation currency (depends on currency parameter).
+   */
   ModelField<Double> amountFiat = new ModelField<>("amount_in_fiat", Double.class, this);
+  /**
+   * invoice transfer commission.
+   */
   ModelField<Double> fee = new ModelField<>("fee", Double.class, this);
+  /**
+   * invoice transfer commission in USD.
+   */
   ModelField<Double> feeUsd = new ModelField<>("fee_usd", Double.class, this);
+  /**
+   * service commission.
+   */
   ModelField<Double> serviceFee = new ModelField<>("service_fee", Double.class, this);
+  /**
+   *  service commission in USD.
+   */
   ModelField<Double> serviceFeeUsd = new ModelField<>("service_fee_usd", Double.class, this);
   ModelField<String> typePayments = new ModelField<>("type_payments", String.class, this);
+  /**
+   *  code of fiat currency in which the invoice was created.
+   */
   ModelField<String> fiatCurrency = new ModelField<>("fiat_currency", String.class, this);
+  /**
+   *  invoice status. When sending a request for invoice creation, it is always created.
+   *  There are also paid, partial, overpaid and canceled statuses.
+   */
   ModelField<String> status = new ModelField<>("status", String.class, this);
+  /**
+   * parameter for mandatory email filling in the invoice.
+   */
   ModelField<Boolean> isEmailRequired = new ModelField<>("is_email_required", Boolean.class, this);
+  /**
+   * link to the invoice page.
+   */
   ModelField<String> link = new ModelField<>("link", String.class, this);
   ModelField<String> invoiceId = new ModelField<>("invoice_id", String.class, this);
+  /**
+   * object of the selected cryptocurrency in the invoice.
+   * It also contains the object network — information about the selected currency network.
+   */
   ModelField<Currency> currency = new ModelField<>("currency", Currency.class, this);
+  /**
+   * information about the project.
+   */
   ModelField<Project> project = new ModelField<>("project", Project.class, this);
+  /**
+   * test invoice sign.
+   */
   ModelField<Boolean> testMode = new ModelField<>("test_mode", Boolean.class, this);
   
   public BillCreated(Map<String, Object> data) {
